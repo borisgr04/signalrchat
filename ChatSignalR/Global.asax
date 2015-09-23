121a76c205ef2888a61f4397f4017f19c3f9727d
'borisgr04@hotmail.com' <admin_sena@SENA>
2015-09-23 17:58:20 -0500

    Primer Cargue....

---
+++ ChatSignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChatSignalR.WebApiApplication" Language="C#" %>
---
+++ ChatSignalR/SignalRChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatSignalR
+{
+    public class SignalRChatHub:Hub
+    {
+        public void broadCastMessage(string msgFrom, string msg)
+        {
+            Clients.All.receiveMessage(msgFrom, msg);
+        }
+    }
+}
